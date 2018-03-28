29b8440b177d8e48545195d23dad514027f53bd8
dxdbl <31200331+dxdbl@users.noreply.github.com>
2018-03-28 17:25:03 +0800

    Update split_file.py

--- split_file.py
+++ split_file.py
@@ -1,20 +1,20 @@
 # coding:utf-8
 ####################################################
 # 程序功能 : 根据文件行数生成新的文件
-# 程序运行说明 : python split_file.py file 1000
+# 程序运行说明 : python split_line.py file 1000
 # 作者 : mhf @ 2018-02-08 16:39
 # 版本 : V1.0
-# 修改时间 :
-# 修改人 :
+# 修改时间 : 2018-03-28
+# 修改人 : mahf
+# 修改说明 : 解决中文编码错误问题
 ####################################################
 import sys
-import os
 
 def se_file_by_line(filename,line_num):
     counter = 0
     findex = 1
     file_count = -1
-    for file_count, line in enumerate(open(filename, 'rU')):
+    for file_count, line in enumerate(open(filename, 'r',encoding='utf-8')):
         pass
     file_count += 1
     print(filename + "  共有%s行数据!" % file_count)
@@ -23,15 +23,15 @@ def se_file_by_line(filename,line_num):
     file_head = filename.split(".")[0]
     print("文件名前缀是 : " + file_head)
 
-    for line in open(filename, "r"):
+    for line in open(filename, "r",encoding='utf-8'):
         if counter == 0:
-            f = open(file_head + "-" + str(findex) + ".txt", "w")
+            f = open(file_head + "-" + str(findex) + ".txt", "w",encoding='utf-8')
             findex += 1
         counter += 1
         f.write(line)
         if counter%line_num == 0 and counter != file_count:
             f.close()
-            f = open(file_head + "-" + str(findex) + ".txt", "w")
+            f = open(file_head + "-" + str(findex) + ".txt", "w",encoding='utf-8')
             findex += 1
     f.close()
 

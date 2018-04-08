d92616f962ddfb1464b4723a74c1c4843439add5
dxdbl <31200331+dxdbl@users.noreply.github.com>
2018-04-08 15:21:24 +0800

    Update line_vs_line.py
    解决了左右行数不一致问题

--- line_vs_line.py
+++ line_vs_line.py
@@ -6,8 +6,20 @@ filename1 = input("请输入文档1名称:\n")
 filename2 = input("请输入文档2名称:\n")
 #filename2 = "right.txt"
 with open(filename1, 'r',encoding='utf-8') as fp1, open(filename2, 'r',encoding='utf-8') as fp2:
-    for i in fp1:
-        j = fp2.readline()
-        print(i, j)
-        f.write(i.strip("\n") + "    " + j)
+    fp1_len = len(fp1.readlines())
+    print(fp1_len)
+    fp2_len = len(fp2.readlines())
+    print(fp2_len)
+if fp1_len >= fp2_len:
+    with open(filename1, 'r', encoding='utf-8') as fp1, open(filename2, 'r', encoding='utf-8') as fp2:
+        for i in fp1:
+            j = fp2.readline()
+            print(i, j)
+            f.write(i.strip("\n") + "    " + j.rstrip("\n") + "\n")
+else:
+    with open(filename1, 'r', encoding='utf-8') as fp1, open(filename2, 'r', encoding='utf-8') as fp2:
+        for i in fp2:
+            j = fp1.readline()
+            print(i, j)
+            f.write(j.strip("\n") + "    " + i.rstrip("\n") + "\n")
 f.close()
